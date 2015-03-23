99702a9e49a6285d51ec6e8987714cda9a562de8
Ayan Lahiri <ayanlahiri@Ayans-MacBook-Pro.local>
2015-03-23 19:22:04 -0400

    new

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-//add script here
+function tipCalculate (){
+	var totalBill = document.getElementById('bill').value;
+
+
+// if(isNan(totalBill))
+
+	var percBtns = document.getElementsByClassName('tip-btn');
+
+	for (var i=0; i<percBtns.length; i++){
+	     if (percBtns[i].checked){
+	     var tipPerc = percBtns[i].value
+	 }
+	}
+
+	if (!isNaN(totalBill)) {
+	  var tipTotal = Math.floor(totalBill * tipPerc * 100) /100;
+	  var tipBox = document.getElementById('tipamount');
+	  tipBox.innerHTML = "$" + tipTotal;
+	} else {
+	  alert('Please enter numbers only');
+
+	}
+
+}
+// var reset = document.getElementsByClassName('tip-btn');
+//    if (bill[i].checked){
+//    	var reset = bill[i].value
+//    }
+
+function resetCalc() {
+	document.getElementById('bill').value
+	='';
+
+var percBtns = document.getElementsByClassName('tip-btn');
+
+for (var i=0; i<percBtns.length; i++){
+     if (percBtns[i].checked){
+     percBtns[i].checked = false;
+
+     }
+  }
+
+ var tipBox = document.getElementById('tipAmount');
+     tipBox.innerHTML = '$0.00';
+
+}
